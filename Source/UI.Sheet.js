5a658ec136fc723ddcb39a7f9e2032eee61d74a9
Aaron Newton <git@clientcide.com>
2010-04-18 17:03:27 -0700

    adding inline docs (until we have external ones - just so others can make sense of all this stuff) adding the ability to define arbitrary groups of sheets

--- Source/UI.Sheet.js
+++ Source/UI.Sheet.js
@@ -8,12 +8,13 @@ provides: UI.Sheet
 ...
 */
 
-(function(){ // http://www.w3.org/TR/CSS21/cascade.html#specificity
+(function(){
 
 var Sheet = UI.Sheet = {};
 
-rules = [];
+var rules = {};
 
+//parses a given selector into an array
 var parseSelector = function(selector){
 	return selector.map(function(chunk){
 		var result = [];
@@ -30,6 +31,8 @@ var parseSelector = function(selector){
 	});
 };
 
+//computes specificity in the same manner that css rules do for a given selector
+//see http://www.w3.org/TR/CSS21/cascade.html#specificity
 var getSpecificity = function(selector){
 	specificity = 0;
 	selector.each(function(chunk){
@@ -40,7 +43,9 @@ var getSpecificity = function(selector){
 	return specificity;
 };
 
-Sheet.define = function(selectors, style){
+//defines the objects (sytle rules) for a given set of selectors
+//and pushes them into an array passed in (the 'where' argument)
+var createRules = function(selectors, style, where){
 	Slick.parse(selectors).expressions.each(function(selector){
 		var rule = {
 			'specificity': getSpecificity(selector),
@@ -48,26 +53,20 @@ Sheet.define = function(selectors, style){
 			'style': {}
 		};
 		for (var p in style) rule.style[p.camelCase()] = style[p];
-		rules.push(rule);
+		where.push(rule);
 	});
-	
-	return this;
 };
 
-var containsAll = function(self, other){
-	return other.every(function(x){
-		return self.contains(x);
-	}, this);
-};
-
-Sheet.lookup = function(selector){
+//retrieves styles for a given selector, merging them into a single
+//object given an array of rules (the 'where' argument)
+var getStyles = function(selector, where) {
 	var style = {};
-	rules.sort(function(a, b){
+	where.sort(function(a, b){
 		return a.specificity - b.specificity;
 	});
 
 	selector = parseSelector(Slick.parse(selector).expressions[0]);
-	rules.each(function(rule){
+	where.each(function(rule){
 		var i = rule.selector.length - 1, j = selector.length - 1;
 		if (!containsAll(selector[j], rule.selector[i])) return;
 		while (i-- > 0){
@@ -79,6 +78,32 @@ Sheet.lookup = function(selector){
 		$mixin(style, rule.style);
 	});
 	return style;
+};
+
+var containsAll = function(self, other){
+	return other.every(function(x){
+		return self.contains(x);
+	}, this);
+};
+
+//returns the rules for a given namespace
+var getRules = function(namespace){
+	if (!namespace) namespace = "default";
+	rules[namespace] = rules[namespace] || [];
+	return rules[namespace];
+};
+
+//defines rules for a given selector
+//takes an object for ART.Sheet "styles" - arbitrary key/value pairs used by widgets
+//namespace (string) is optional; allows for the use of ART.Sheet with arbitrary style groups
+Sheet.define = function(selectors, style, namespace){
+	createRules(selectors, style, getRules(namespace));
+	return this;
+};
+
+//returns the "style" object for a given selector
+Sheet.lookup = function(selector, namespace){
+	return getStyles(selector, getRules(namespace));
 };
 
 })();
